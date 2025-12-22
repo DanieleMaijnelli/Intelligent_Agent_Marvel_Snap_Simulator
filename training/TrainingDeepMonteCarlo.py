b4def127fbbe97fdda7bc04908fdb2a756e8eda6
DanieleMaijnelli <dani.maijnelli2002@gmail.com>
2025-12-22 23:34:02 +0100

    feature: added evaluation only directory and file

--- training/TrainingDeepMonteCarlo.py
+++ training/TrainingDeepMonteCarlo.py
@@ -227,9 +227,6 @@ def train_deep_monte_carlo_with_logging(
             predicted_q_tensor = q_network(state_action_tensor)
             loss_value = loss_function(predicted_q_tensor, target_tensor)
 
-            print("Trova l'errore")
-            print(predicted_q_tensor.shape, target_tensor.shape)
-
             optimizer.zero_grad()
             loss_value.backward()
             optimizer.step()
