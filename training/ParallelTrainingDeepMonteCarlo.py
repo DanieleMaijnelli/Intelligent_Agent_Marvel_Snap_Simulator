a7a0dd1f32789a5248a22d389da2ad14825194ef
DanieleMaijnelli <dani.maijnelli2002@gmail.com>
2026-01-29 15:01:00 +0100

    experiment: parallelization implemented

--- training/ParallelTrainingDeepMonteCarlo.py
+++ training/ParallelTrainingDeepMonteCarlo.py
@@ -167,7 +167,7 @@ def generate_episode(environment, q_network, epsilon, episode_index, decay_episo
     )
 
 
-def train_deep_monte_carlo_with_logging(
+def train_deep_monte_carlo_parallel_with_logging(
     number_of_episodes,
     learning_rate=1e-4,
     epsilon_start=0.9,
@@ -178,18 +178,17 @@ def train_deep_monte_carlo_with_logging(
     decay_fraction=0.5,
     log_csv_path=None,
     save_model_path=None,
-    number_of_total_processes=4
+    number_of_total_processes=4,
 ):
     if seed_value is not None:
         set_global_seed(seed_value)
+
     start_time_seconds = time.time()
 
     input_dimension = get_input_dimension()
     q_network = QNetworkB(input_dimension)
     optimizer = optim.Adam(q_network.parameters(), lr=learning_rate)
     loss_function = nn.MSELoss()
-
-    environment = SingleAgentTestEnvironment()
 
     training_loss_history = []
     ally_win_rate_history = []
@@ -226,106 +225,151 @@ def train_deep_monte_carlo_with_logging(
 
     episode_index = 0
     decay_episodes = int(number_of_episodes * decay_fraction)
+    decay_episodes_enemy = int(number_of_episodes * 0.5)
 
-    while episode_index < number_of_episodes:
-        if episode_index < decay_episodes:
-            epsilon = epsilon_end + (epsilon_start - epsilon_end) * (
-                1.0 - float(episode_index) / float(decay_episodes - 1)
-            )
-        else:
-            epsilon = epsilon_end
+    try:
+        while episode_index < number_of_episodes:
+            q_network.eval()
+            q_network_state_dictionary_bytes = serialize_state_dictionary(q_network.state_dict())
+            q_network.train()
 
-        decay_episodes_enemy = int(number_of_episodes * 0.5)
+            tasks_to_send = []
+            local_actor_index = 0
+            while local_actor_index < number_of_actor_processes and episode_index + local_actor_index < number_of_episodes:
+                current_episode_index = episode_index + local_actor_index
 
-        q_network.eval()
-        (
-            episode_ally_state_actions,
-            episode_enemy_state_actions,
-            ally_return_list,
-            enemy_return_list,
-            final_reward_ally,
-            final_reward_enemy,
-        ) = generate_episode(environment, q_network, epsilon, episode_index, decay_episodes_enemy)
-        q_network.train()
+                if current_episode_index < decay_episodes:
+                    epsilon = epsilon_end + (epsilon_start - epsilon_end) * (
+                        1.0 - float(current_episode_index) / float(decay_episodes - 1)
+                    )
+                else:
+                    epsilon = epsilon_end
 
-        training_state_action_list = []
-        target_return_list = []
-
-        for state_action_vector, target_return in zip(episode_ally_state_actions, ally_return_list):
-            training_state_action_list.append(state_action_vector)
-            target_return_list.append(target_return)
-
-        for state_action_vector, target_return in zip(episode_enemy_state_actions, enemy_return_list):
-            training_state_action_list.append(state_action_vector)
-            target_return_list.append(target_return)
-
-        loss_value_float = 0.0
-
-        if len(training_state_action_list) > 0:
-            state_action_array = numpy.stack(training_state_action_list).astype(
-                numpy.float32
-            )
-            target_array = numpy.array(target_return_list, dtype=numpy.float32)
-
-            state_action_tensor = torch.from_numpy(state_action_array)
-            target_tensor = torch.from_numpy(target_array)
-
-            predicted_q_tensor = q_network(state_action_tensor)
-            loss_value = loss_function(predicted_q_tensor, target_tensor)
-
-            optimizer.zero_grad()
-            loss_value.backward()
-            optimizer.step()
-
-            loss_value_float = float(loss_value.item())
-            training_loss_history.append(loss_value_float)
-
-        ally_win_rate = ""
-        enemy_win_rate = ""
-        tie_rate = ""
-        elapsed_minutes = ""
-        deck_pair_ally_win_rate_value_list = []
-        deck_pair_value_index = 0
-        while deck_pair_value_index < 16:
-            deck_pair_ally_win_rate_value_list.append("")
-            deck_pair_value_index += 1
-
-        if evaluation_interval is not None and evaluation_interval > 0:
-            if (episode_index + 1) % evaluation_interval == 0:
-                q_network.eval()
-                eval_results = evaluate_against_chosen_opponent(
-                    q_network, evaluation_games, epsilon_agent=0.0
+                tasks_to_send.append(
+                    {
+                        "q_network_state_dictionary_bytes": q_network_state_dictionary_bytes,
+                        "epsilon": epsilon,
+                        "episode_index": current_episode_index,
+                        "decay_episodes_enemy": decay_episodes_enemy,
+                    }
                 )
-                q_network.train()
-                elapsed_minutes = (time.time() - start_time_seconds) / 60.0
-                ally_win_rate = eval_results["ally_win_rate"]
-                enemy_win_rate = eval_results["enemy_win_rate"]
-                tie_rate = eval_results["tie_rate"]
-                deck_pair_ally_win_rate_value_list = extract_deck_pair_ally_win_rate_list(eval_results)
+                local_actor_index += 1
 
-                ally_win_rate_history.append(ally_win_rate)
-                enemy_win_rate_history.append(enemy_win_rate)
-                tie_rate_history.append(tie_rate)
+            for task_dictionary in tasks_to_send:
+                actor_task_queue.put(task_dictionary)
 
-        if csv_writer is not None:
-            write_training_csv_row(
-                csv_writer,
-                episode_index + 1,
-                elapsed_minutes,
-                epsilon,
-                loss_value_float,
-                final_reward_ally,
-                final_reward_enemy,
-                ally_win_rate,
-                enemy_win_rate,
-                tie_rate,
-                deck_pair_ally_win_rate_value_list,
-            )
+            received_result_list = []
+            received_results_count = 0
+            while received_results_count < len(tasks_to_send):
+                received_result_list.append(learner_result_queue.get())
+                received_results_count += 1
 
-        episode_index += 1
+            received_result_list.sort(key=lambda result_dictionary: result_dictionary["episode_index"])
 
-    if csv_file is not None:
-        csv_file.close()
+            for result_dictionary in received_result_list:
+                current_episode_index = int(result_dictionary["episode_index"])
+
+                episode_ally_state_actions = result_dictionary["episode_ally_state_actions"]
+                episode_enemy_state_actions = result_dictionary["episode_enemy_state_actions"]
+                ally_return_list = result_dictionary["ally_return_list"]
+                enemy_return_list = result_dictionary["enemy_return_list"]
+                final_reward_ally = result_dictionary["final_reward_ally"]
+                final_reward_enemy = result_dictionary["final_reward_enemy"]
+
+                training_state_action_list = []
+                target_return_list = []
+
+                for state_action_vector, target_return in zip(episode_ally_state_actions, ally_return_list):
+                    training_state_action_list.append(state_action_vector)
+                    target_return_list.append(target_return)
+
+                for state_action_vector, target_return in zip(episode_enemy_state_actions, enemy_return_list):
+                    training_state_action_list.append(state_action_vector)
+                    target_return_list.append(target_return)
+
+                loss_value_float = 0.0
+
+                if len(training_state_action_list) > 0:
+                    state_action_array = numpy.stack(training_state_action_list).astype(numpy.float32)
+                    target_array = numpy.array(target_return_list, dtype=numpy.float32)
+
+                    state_action_tensor = torch.from_numpy(state_action_array)
+                    target_tensor = torch.from_numpy(target_array)
+
+                    q_network.train()
+                    predicted_q_tensor = q_network(state_action_tensor)
+                    loss_value = loss_function(predicted_q_tensor, target_tensor)
+
+                    optimizer.zero_grad()
+                    loss_value.backward()
+                    optimizer.step()
+
+                    loss_value_float = float(loss_value.item())
+                    training_loss_history.append(loss_value_float)
+
+                ally_win_rate = ""
+                enemy_win_rate = ""
+                tie_rate = ""
+                elapsed_minutes = ""
+                deck_pair_ally_win_rate_value_list = []
+                deck_pair_value_index = 0
+                while deck_pair_value_index < 16:
+                    deck_pair_ally_win_rate_value_list.append("")
+                    deck_pair_value_index += 1
+
+                if evaluation_interval is not None and evaluation_interval > 0:
+                    if (current_episode_index + 1) % evaluation_interval == 0:
+                        q_network.eval()
+                        eval_results = evaluate_against_chosen_opponent(
+                            q_network, evaluation_games, epsilon_agent=0.0
+                        )
+                        q_network.train()
+                        elapsed_minutes = (time.time() - start_time_seconds) / 60.0
+                        ally_win_rate = eval_results["ally_win_rate"]
+                        enemy_win_rate = eval_results["enemy_win_rate"]
+                        tie_rate = eval_results["tie_rate"]
+                        deck_pair_ally_win_rate_value_list = extract_deck_pair_ally_win_rate_list(eval_results)
+
+                        ally_win_rate_history.append(ally_win_rate)
+                        enemy_win_rate_history.append(enemy_win_rate)
+                        tie_rate_history.append(tie_rate)
+
+                if csv_writer is not None:
+                    if current_episode_index < decay_episodes:
+                        epsilon_for_csv = epsilon_end + (epsilon_start - epsilon_end) * (
+                            1.0 - float(current_episode_index) / float(decay_episodes - 1)
+                        )
+                    else:
+                        epsilon_for_csv = epsilon_end
+
+                    write_training_csv_row(
+                        csv_writer,
+                        current_episode_index + 1,
+                        elapsed_minutes,
+                        epsilon_for_csv,
+                        loss_value_float,
+                        final_reward_ally,
+                        final_reward_enemy,
+                        ally_win_rate,
+                        enemy_win_rate,
+                        tie_rate,
+                        deck_pair_ally_win_rate_value_list,
+                    )
+
+            episode_index += len(tasks_to_send)
+
+    finally:
+        # Stop pulito degli actor
+        stop_index = 0
+        while stop_index < number_of_actor_processes:
+            actor_task_queue.put({"command": "stop"})
+            stop_index += 1
+
+        for actor_process in actor_process_list:
+            actor_process.join()
+
+        if csv_file is not None:
+            csv_file.close()
 
     if save_model_path is not None:
         save_q_network(q_network, save_model_path)
@@ -341,8 +385,8 @@ def train_deep_monte_carlo_with_logging(
 
 
 if __name__ == "__main__":
-    number_of_episodes = 2000000
-    results = train_deep_monte_carlo_with_logging(
+    number_of_episodes = 1300000
+    results = train_deep_monte_carlo_parallel_with_logging(
         number_of_episodes=number_of_episodes,
         learning_rate=5e-5,
         epsilon_start=0.9,
@@ -353,6 +397,7 @@ if __name__ == "__main__":
         decay_fraction=0.35,
         log_csv_path=f"training_log_DMC_{number_of_episodes}_episodes.csv",
         save_model_path=f"trained_q_network_DMC_{number_of_episodes}_episodes.pt",
+        number_of_total_processes=4
     )
 
     trained_q_network = results["q_network"]
